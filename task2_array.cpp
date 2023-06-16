496a5e004df46d5e2910539a7fc5bdedd53f36fc
SeveredAsif <109129903+SeveredAsif@users.noreply.github.com>
2023-06-16 13:06:25 +0600

    Create task2_array.cpp

---
+++ task2_array.cpp
@@ -0,0 +1,67 @@
+#include"Arr.h"
+#include<iostream>
+using namespace std;
+
+void List ::append(int a) {
+    int curr = currPos();
+    moveToPos(len);
+    insert(a);
+    moveToPos(curr);
+  }
+
+void List::clear(){
+  movetoStart();
+  while(length()){
+    remove();
+    next();
+  }
+}
+int List::Search(int a){
+  int l = length();
+  int pos = currPos();
+  movetoStart();
+  for (int i=0;i<l;i++){
+      if(getValue()==a){
+        moveToPos(pos);
+        return i;
+      }
+      next();
+  }
+  return -1;
+}
+
+
+int main() {
+  List a;
+  int K, X;
+  cin >> K >> X;
+  a.init(K,X);
+  for (int i = 0; i < K; i++) {
+    int input;
+    cin >> input;
+    a.insert(input);
+    a.next();
+  }
+  a.movetoStart();
+  a.print();  
+  while (1) {
+    int Q, P;
+    cin >> Q >> P;
+    if (Q == 0) {
+      break;
+    } else if (Q == 1) {
+      a.append(P);
+      a.print();
+    } else if (Q == 2) {
+      a.clear();
+      a.print();
+      a.moveToPos(0);
+    } else if (Q == 3) {
+      int s = a.Search(3);
+      cout<<s<<endl;
+      int pos = a.currPos();
+      a.print();
+    } 
+
+  }
+}  

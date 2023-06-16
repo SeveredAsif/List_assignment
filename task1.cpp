70377a4bdaafbff97311fe9796c7695b99527def
SeveredAsif <109129903+SeveredAsif@users.noreply.github.com>
2023-06-16 12:43:07 +0600

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,227 @@
+#include"LL.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+  List a;
+  int K, X;
+  cin >> K >> X;
+  a.init();
+  for (int i = 0; i < K; i++) {
+    int input;
+    cin >> input;
+    a.insert(input);
+    a.next();
+  }
+  a.movetoStart();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;  
+  while (1) {
+    int Q, P;
+    cin >> Q >> P;
+    if (Q == 0) {
+      break;
+    } else if (Q == 1) {
+      a.insert(P);
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 2) {
+      int x = a.remove();
+      cout<<"removed value: "<<x<<endl;
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 3) {
+      a.movetoStart();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 4) {
+      a.movetoEnd();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 5) {
+      a.prev();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 6) {
+      a.next();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 7) {
+      int x=a.length();
+      cout<<"length is "<<x<<endl;
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 8) {
+      int pos = a.currPos();
+      cout<<"current position is: "<<pos<<endl;
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+
+    } else if (Q == 9) {
+      a.moveToPos(P);
+      int pos = a.currPos();
+      a.movetoStart();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    } else if (Q == 10) {
+      int x = a.getValue();
+      cout<<"value is : "<<x<<endl;
+      a.print();
+      int pos = a.currPos();
+      cout << "<";
+      a.movetoStart();
+      int len = a.length();
+      for (int i = 0; i <= len; i++) {
+        if (i == pos) {
+          cout << "|";
+        }
+        if (i != len) cout << a.getValue();
+        if (i < len - 1) {
+          cout << ",";
+        }
+        a.next();
+      }
+      a.moveToPos(pos);
+      cout << ">" << endl;
+    }
+  }
+
+}

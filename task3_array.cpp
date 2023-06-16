f38f33cd18dbf401060ad46088a1c014fc12c2a6
SeveredAsif <109129903+SeveredAsif@users.noreply.github.com>
2023-06-16 14:14:37 +0600

    Create task3_array.cpp

---
+++ task3_array.cpp
@@ -0,0 +1,85 @@
+#include"Arr.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+void print(List* CRS,int Y){
+    for(int i=0;i<Y;i++){
+        int l = CRS[i].length();
+        CRS[i].movetoStart();
+        while(l--){
+            cout<<CRS[i].getValue()<<"\t";
+            CRS[i].next();
+        }
+        cout<<endl;
+ 
+    }
+}
+
+int main(){
+    int X,Y,Z;
+    cin>>X>>Y>>Z;
+    List *CRS = new List[Y];
+    for(int i=0;i<Y;i++){
+        CRS[i].init(Z,Z);
+    }
+    int M = Y;
+    while(M--){
+        int index;
+        int car;
+        char c;
+        
+        cin>>index;
+        index -= 1;
+        cin.get(c);
+        
+        while(c!='\n'){
+            cin>>car;
+            CRS[index].insert(car);
+            CRS[index].next();
+            cin.get(c);
+        }
+    }
+    print(CRS,Y);
+
+    while(1){
+        string cmd;
+        cin>>cmd;
+        if (cmd=="req"){
+            int flag = 0;
+            for(int i=0;i<Y;i++){
+                if(CRS[i].length()!=0){
+                    flag = 1;
+                    CRS[i].movetoStart();
+                    CRS[i].remove();
+                    print(CRS,Y);
+                    break;
+                }
+            }
+            if(flag==0){
+                cout<<"No cars are available"<<endl;
+            }
+        }
+        if(cmd=="ret"){
+            int serial;
+            cin>>serial;
+            int flag =0;
+            for(int i = Y-1;i>=0;i--){
+                if(CRS[i].length()<Z){
+                    flag =1;
+                    CRS[i].movetoEnd();
+                    CRS[i].insert(serial);
+                    print(CRS,Y);
+                    break;
+                }
+            }
+            if(flag==0){
+                cout<<"All garages are full"<<endl;
+            }
+        }
+        if (cmd=="end"){
+            break;
+        }
+    }
+    delete[] CRS;
+}
